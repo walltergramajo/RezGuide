423a42b61e3c832bba9cb98029077682e50fda4c
walltergramajo <waltergramajo@yahoo.ca>
2014-10-24 13:14:21 -0400

    Pagination Positioning
    Changed the position of the Pagination function to not sit inside of a
    scrollable div.

--- WebApp_Main/pd_account_edit.php
+++ WebApp_Main/pd_account_edit.php
@@ -23,110 +23,112 @@
             </div>
         </div>
     </header>
-    <section class="mainContent">
-    <div class="row" id="editAccountContent">
-    	<div class="small-4 columns">
-    		<div class="small-12 columns text-center">
-        		<img id="profilePicEdit" src="img/cms-users.png" width="250" alt="Account Picture">
-       		</div>
-        	<div class="small-12 columns text-center">
-        		<h3 class="headerInfo" id="firstName">First Name </h3>
-       		</div>
-        	<div class="small-12 columns text-center">
-        		<h3 class="headerInfo" id="lastName">Last Name </h3>
+    <section>
+        <div class="mainContent">
+        <div class="row" id="editAccountContent">
+        	<div class="small-4 columns">
+        		<div class="small-12 columns text-center">
+            		<img id="profilePicEdit" src="img/cms-users.png" width="250" alt="Account Picture">
+           		</div>
+            	<div class="small-12 columns text-center">
+            		<h3 class="headerInfo" id="firstName">First Name </h3>
+           		</div>
+            	<div class="small-12 columns text-center">
+            		<h3 class="headerInfo" id="lastName">Last Name </h3>
+            	</div>
+            	<div class="small-12 columns text-center">
+            		<h3 class="headerInfo" id="programName">Program </h3>
+            	</div>
         	</div>
-        	<div class="small-12 columns text-center">
-        		<h3 class="headerInfo" id="programName">Program </h3>
-        	</div>
-    	</div>
-    	<div class="small-8 columns">
-        	<div class="profileInfo">
-    			<div class="row">
-        			<div class="small-7 columns">
-            			<h3 class="sectionLabel">CONTACT INFORMATION</h3>
+        	<div class="small-8 columns">
+            	<div class="profileInfo">
+        			<div class="row">
+            			<div class="small-7 columns">
+                			<h3 class="sectionLabel">CONTACT INFORMATION</h3>
+                		</div>
+                		<div class="small-4 small-push-2 columns">
+                			<a href="#"><h4 class="sectionLabel">edit profile</h4></a>
+                		</div>
             		</div>
-            		<div class="small-4 small-push-2 columns">
-            			<a href="#"><h4 class="sectionLabel">edit profile</h4></a>
-            		</div>
-        		</div>
-                <div class="row">
-    				<div class="small-12 accountLine"></div>
-    			</div>
-                <form>
-                	<div class="row">
-                		<div class="small-2 columns">
-                    		<label class="labelInfo">cellphone#</label>
-                    	</div>
-                        <div class="small-4 small-push-1 columns">
-                        	<input type="text" placeholder="Cellphone #"/>
-                        </div>
-                        <div class="small-2 small-push-1 columns">
-                    		<label class="labelInfo">student#</label>
-                    	</div>
-                        <div class="small-3 columns">
-                        	<input type="text" placeholder="student #"/>
-                        </div>
-                	</div>
                     <div class="row">
-                    	<div class="small-3 columns">
-                        	<label class="labelInfo">build/floor/room</label>
-                        </div>
-                        <div class="small-9 columns">
-                        	<input type="text" placeholder="build/floor/room"/>
-                        </div>
-                    </div>
-                    <div class="row">
-        				<div class="small-7 columns">
-            				<h3 class="sectionLabel" id="socialMedia">SOCIAL MEDIA</h3>
-            			</div>
-            			
+        				<div class="small-12 accountLine"></div>
         			</div>
-                	<div class="row">
-    					<div class="small-12 accountLine"></div>
-    				</div>
-                    <div class="row">
-                    	<div class="small-2 columns">
-                        	<label class="labelInfo">facebook</label>
+                    <form>
+                    	<div class="row">
+                    		<div class="small-2 columns">
+                        		<label class="labelInfo">cellphone#</label>
+                        	</div>
+                            <div class="small-4 small-push-1 columns">
+                            	<input type="text" placeholder="Cellphone #"/>
+                            </div>
+                            <div class="small-2 small-push-1 columns">
+                        		<label class="labelInfo">student#</label>
+                        	</div>
+                            <div class="small-3 columns">
+                            	<input type="text" placeholder="student #"/>
+                            </div>
+                    	</div>
+                        <div class="row">
+                        	<div class="small-3 columns">
+                            	<label class="labelInfo">build/floor/room</label>
+                            </div>
+                            <div class="small-9 columns">
+                            	<input type="text" placeholder="build/floor/room"/>
+                            </div>
                         </div>
-                        <div class="small-9 columns">
-                        	<input type="text" placeholder="facebook"/>
+                        <div class="row">
+            				<div class="small-7 columns">
+                				<h3 class="sectionLabel" id="socialMedia">SOCIAL MEDIA</h3>
+                			</div>
+                			
+            			</div>
+                    	<div class="row">
+        					<div class="small-12 accountLine"></div>
+        				</div>
+                        <div class="row">
+                        	<div class="small-2 columns">
+                            	<label class="labelInfo">facebook</label>
+                            </div>
+                            <div class="small-9 columns">
+                            	<input type="text" placeholder="facebook"/>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                    	<div class="small-2 columns">
-                        	<label class="labelInfo">twitter</label>
+                        <div class="row">
+                        	<div class="small-2 columns">
+                            	<label class="labelInfo">twitter</label>
+                            </div>
+                            <div class="small-9 columns">
+                            	<input type="text" placeholder="twitter"/>
+                            </div>
                         </div>
-                        <div class="small-9 columns">
-                        	<input type="text" placeholder="twitter"/>
+                        <div class="row">
+                        	<div class="small-2 columns">
+                            	<label class="labelInfo">linkedIn</label>
+                            </div>
+                            <div class="small-9 columns">
+                            	<input type="text" placeholder="linkedIn"/>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                    	<div class="small-2 columns">
-                        	<label class="labelInfo">linkedIn</label>
-                        </div>
-                        <div class="small-9 columns">
-                        	<input type="text" placeholder="linkedIn"/>
-                        </div>
-                    </div>
-                </form>
-             </div><!--End Profile Info-->
-     	</div>
-    </div><!--End Row--> 
-    	<div class="row">
-        	<div class="small-5 small-centered columns">
-            	<div class="profileNav">
-            		<ul class="pagination"> 
-                		<li class="arrow unavailable"><a href=""><img id="prevArrow" src="img/profilePrevArrow.png" height="20px" width="20px" alt="previous"></a></li> 
-                    	<li><a href="pd_account_edit.php"><img src="img/profileCurrentPage.png" height="20px" width="20px" alt="currentPage"></a></li> 
-                    	<li><a href="pd_account_edit_p2.php"><img src="img/profilePageIcon.png" height="20px" width="20px" alt="emptyPage"></a></li> 
-                    	 <li><a href="pd_account_balance.php"><img src="img/profilePageIcon.png" height="20" width="20" alt="currentPage"></a></li> 
-                    	<li><a href="pd_account_edit_p2.php"><img id="nextArrow" src="img/profileNextArrow.png" height="20px" width="20px" alt="previous"></a></li> 
-                 	</ul>
+                    </form>
+                 </div><!--End Profile Info-->
+         	</div>
+        </div><!--End Row--> 
+        </div><!--End MainContent-->
+
+        <div class="row">
+            <div class="small-5 small-centered columns">
+                <div class="profileNav">
+                    <ul class="pagination"> 
+                        <li class="arrow unavailable"><a href=""><img id="prevArrow" src="img/profilePrevArrow.png" height="20px" width="20px" alt="previous"></a></li> 
+                        <li><a href="pd_account_edit.php"><img src="img/profileCurrentPage.png" height="20px" width="20px" alt="currentPage"></a></li> 
+                        <li><a href="pd_account_edit_p2.php"><img src="img/profilePageIcon.png" height="20px" width="20px" alt="emptyPage"></a></li> 
+                        <li><a href="pd_account_edit_p2.php"><img id="nextArrow" src="img/profileNextArrow.png" height="20px" width="20px" alt="previous"></a></li> 
+                    </ul>
                 </div>
             </div>
-        </div>   
-    <div class="filler"> </div>
+        </div>
     </section>
+ 
     <footer>
         <?php
             include('includes/footer.php');
--- WebApp_Main/pd_account_edit_p2.php
+++ WebApp_Main/pd_account_edit_p2.php
@@ -24,60 +24,59 @@
         </div>
     </header>
 
-<section class="mainContent">
-<div id="editAccountContent" class="row">
-    <div class="small-9 small-centered columns">
-            <h3 class="sectionLabel">RESIDENCE STATUS</h3>
-        </div>
-    </div>
-    <div class="row">
-        <div class="small-10 small-centered columns">
-            <div class="accountLine">
-            </div>
-        </div>
-    </div>
-	
-    <form>
-        <div class="row">
+<section>
+    <div class="mainContent">
+        <div id="editAccountContent" class="row">
             <div class="small-9 small-centered columns">
-                <div class="small-6 columns">
-                    <div class="small-2 small-pull-1 columns">
-                        <label class="labelInfo">credit</label>
-                    </div>
-                    <div class="small-8 small-pull-1 columns">
-                    <input type="text" placeholder="credit amount"/>
-                    </div>
-                 </div>
-                 <div class="small-6 small-pull-1 columns">
-                    
-                        <div class="small-2 columns">
-                            <label class="labelInfo">debt</label>
-                        </div>
-                        <div class="small-8 small-pull-1 columns">
-                            <input type="text" placeholder="amount owed"/>
-                        </div>
-                    
-                 </div>
-           </div>
-        </div>
-    </form>
-</div>    <!--End EditAccountContent-->
- 
-             
-    	<div class="row">
-        	<div class="small-5 small-centered columns">
-            	<div class="profileNav">
-            		<ul class="pagination"> 
-                		<li><a href="pd_account_edit.php"><img id="prevArrow" src="img/profilePrevArrow.png" height="20" width="20" alt="previous"></a></li> 
-                    	<li><a href="pd_account_edit.php"><img src="img/profilePageIcon.png" height="20" width="20" alt="emptyPage"></a></li> 
-                    	<li><a href="pd_account_edit_p2.php"><img src="img/profileCurrentPage.png" height="20" width="20" alt="currentPage"></a></li>  
-                    	<li class="arrow unavailable"><a href="#"><img id="nextArrow" src="img/profileNextArrow.png" height="20" width="20" alt="previous"></a></li> 
-                 	</ul>
+                    <h3 class="sectionLabel">RESIDENCE STATUS</h3>
                 </div>
             </div>
-        </div>   
-    <div class="filler"> </div>
-    </section>
+            <div class="row">
+                <div class="small-10 small-centered columns">
+                    <div class="accountLine">
+                    </div>
+                </div>
+            </div>
+        	
+            <form>
+                <div class="row">
+                    <div class="small-9 small-centered columns">
+                        <div class="small-6 columns">
+                            <div class="small-2 small-pull-1 columns">
+                                <label class="labelInfo">credit</label>
+                            </div>
+                            <div class="small-8 small-pull-1 columns">
+                            <input type="text" placeholder="credit amount"/>
+                            </div>
+                         </div>
+                         <div class="small-6 small-pull-1 columns">
+                            
+                                <div class="small-2 columns">
+                                    <label class="labelInfo">debt</label>
+                                </div>
+                                <div class="small-8 small-pull-1 columns">
+                                    <input type="text" placeholder="amount owed"/>
+                                </div>
+                            
+                         </div>
+                   </div>
+                </div>
+            </form>
+        </div>    <!--End EditAccountContent-->
+    </div><!--End mainContent-->
+    <div class="row">
+            <div class="small-5 small-centered columns">
+                <div class="profileNav">
+                    <ul class="pagination"> 
+                        <li><a href="pd_account_edit.php"><img id="prevArrow" src="img/profilePrevArrow.png" height="20" width="20" alt="previous"></a></li> 
+                        <li><a href="pd_account_edit.php"><img src="img/profilePageIcon.png" height="20" width="20" alt="emptyPage"></a></li> 
+                        <li><a href="pd_account_edit_p2.php"><img src="img/profileCurrentPage.png" height="20" width="20" alt="currentPage"></a></li>  
+                        <li class="arrow unavailable"><a href="#"><img id="nextArrow" src="img/profileNextArrow.png" height="20" width="20" alt="previous"></a></li> 
+                    </ul>
+                </div>
+            </div>
+        </div>  
+    </section> 
     <footer>
         <?php
             include('includes/footer.php');
