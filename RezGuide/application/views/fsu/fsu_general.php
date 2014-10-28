930aa5090abf79c417e7f2bf990424fd5512de9a
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-10-28 15:47:42 -0400

    Added for each loops
    Added foreach loops to echo out content. Pulled from McHale’s version.

--- RezGuide/application/views/fsu/fsu_general.php
+++ RezGuide/application/views/fsu/fsu_general.php
@@ -9,12 +9,18 @@
             <div class="small-8 small-centered columns">
             	<div class="accordionScroll">
                 	<dl class="accordion" data-accordion>
-                    	<dd class="accordionBottom">
-                			<a href="#panel1" class="accordionLink accordionTitle" onClick="highlightLink2(this);">Content 1 <span class="right accordionDropDownIcon"><img class="imgswap" src="<?php echo base_url(); ?>img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
-                			<div id="panel1" class="content"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                			</div>
-              			</dd>
-                        <dd class="accordionBottom">
+                        <?php foreach ($results as $row): ?>
+                        	<dd class="accordionBottom">
+                    			<a href="#panel<?php echo $row->fsuinfo_id ?>" class="accordionLink accordionTitle" onClick="highlightLink2(this);"><?php echo $row->fsuinfo_title ?><span class="right accordionDropDownIcon"><img class="imgswap" src="<?php echo base_url(); ?>img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
+                                <div id="panel<?php echo $row->fsuinfo_id ?>" class="content">
+                                    <p class="complaintDate"><?php echo $row->fsuinfo_date ?></p>
+                                    <p class="location"><?php echo $row->fsuinfo_location ?></p>
+                                    <div><?php echo $row->fsuinfo_description ?></div>
+                                    <p><a href="<?php echo $row->fsuinfo_link ?>"><?php echo $row->fsuinfo_link ?></a></p>
+                                </div>
+                  			</dd>
+                        <?php endforeach; ?>
+                        <!-- <dd class="accordionBottom">
                 			<a href="#panel2" class="accordionLink accordionTitle" onClick="highlightLink2(this);">Content 2 <span class="right accordionDropDownIcon"><img class="imgswap" src="<?php echo base_url(); ?>img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
                 			<div id="panel2" class="content"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 			</div>
@@ -28,7 +34,7 @@
                 			<a href="#panel4" class="accordionLink accordionTitle" onClick="highlightLink2(this);">Content 4 <span class="right accordionDropDownIcon"><img class="imgswap" src="<?php echo base_url(); ?>img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
                 			<div id="panel4" class="content"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 			</div>
-              			</dd>
+              			</dd> -->
                     </dl>
                 </div>
             </div>
