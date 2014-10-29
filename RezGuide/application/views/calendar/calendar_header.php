7e0dfb210926efa4f8663c94deaa93d048a4561c
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-10-29 13:14:15 -0400

    Added new views for calendar
    Added main calendar view, calendar header, and script views.

---
+++ RezGuide/application/views/calendar/calendar_header.php
@@ -0,0 +1,12 @@
+<header>
+	<div class="row" id="headerRow">
+    	<div class="small-3 columns" id="mainLogo">
+        	<img src="<?php echo base_url(); ?>img/FINAL_LOGO.png" alt="Reznet">
+        </div>
+       <div class="small-9 columns" id="pageTitle">
+        	<div id="headerContainerSolo">
+        	<h2 id="titleHeaderSolo" class="text-right header">Calendar</h2>
+            </div>
+        </div>
+    </div>
+</header>
