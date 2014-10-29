a0e25bfc645206875ad40fbab6f5bf81ad368424
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-10-29 13:13:27 -0400

    Changed Calendar controller
    Changed main function to index. commented out old code, added new loads
    for new views.

--- RezGuide/application/controllers/calendar.php
+++ RezGuide/application/controllers/calendar.php
@@ -2,24 +2,33 @@
 
 class Calendar extends CI_Controller {
 	
-	public function showCal(){
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function index(){
 		$data['pgTitle'] = "Calendar";
 		$this->load->view('templates/head', $data);
+		$this->load->view('calendar/calendar_header');
 
-		$prefs = array(
-			'show_next_prev' => TRUE,
-			'next_prev_url' => base_url().'index.php/calendar/showCal'
-		);
+		$this->load->view('calendar/calendar');
 
-		$this->load->library('calendar', $prefs);
+		// $prefs = array(
+		// 	'show_next_prev' => TRUE,
+		// 	'next_prev_url' => base_url().'index.php/calendar/showCal'
+		// );
+
+		// $this->load->library('calendar', $prefs);
 
 		
-		$this->load->view('building/bd_header');
+		// $this->load->view('building/bd_header');
 
-		$data['calendar'] = $this->calendar->generate($this->uri->segment(3),$this->uri->segment(4));
-		$this->load->view('calendar_view',$data);
+		// $data['calendar'] = $this->calendar->generate($this->uri->segment(3),$this->uri->segment(4));
+		// $this->load->view('calendar_view',$data);
 
 		$this->load->view('templates/footer');
+		$this->load->view('includes/calendar_datepicker');
+		// $this->load->view('includes/datepicker');
 		$this->load->view('templates/close');
 	}
 }
