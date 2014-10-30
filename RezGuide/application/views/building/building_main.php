7d67cba75c4faa324681f0c0d2a7656909ee01ee
walltergramajo <waltergramajo@yahoo.ca>
2014-10-30 17:04:56 -0400

    Building Menu Icons
    Changed Building Menu Icons to the dark color and created the hover
    color ones.

--- RezGuide/application/views/building/building_main.php
+++ RezGuide/application/views/building/building_main.php
@@ -4,19 +4,19 @@
             <div class="small-12 columns">
                 <div class="small-4 columns text-center">
                     <a class="botAnchor" href="<?php echo base_url(); ?>index.php/classifieds">
-                        <img onMouseOver="swapDark('0')" onMouseOut="swapLight('0')" class="botImg" src="<?php echo base_url(); ?>img/classifiedsIcon.png" alt="Account">
+                        <img onMouseOver="swapDark('0')" onMouseOut="swapLight('0')" class="botImg" src="<?php echo base_url(); ?>img/WA_classifieds.png" alt="Classifieds">
                         <p class="dashboardText"> Classifieds </p>
                     </a>
                 </div>
                 <div class="small-4 columns text-center">
                     <a class="botAnchor" href="<?php echo base_url(); ?>index.php/building/garbage">
-                        <img onMouseOver="swapDark('1')" onMouseOut="swapLight('1')" class="botImg" src="<?php echo base_url(); ?>img/buildingEventsIcon.png" alt="Requests">
+                        <img onMouseOver="swapDark('1')" onMouseOut="swapLight('1')" class="botImg" src="<?php echo base_url(); ?>img/WA_buildingInfo.png" alt="Building Info">
                         <p class="dashboardText"> Building <br>Info </p>
                     </a>
                 </div>
                 <div class="small-4 columns text-center">
                     <a class="botAnchor" href="<?php echo base_url(); ?>index.php/events">
-                        <img onMouseOver="swapDark('2')" onMouseOut="swapLight('2')" class="botImg" src="<?php echo base_url(); ?>img/eventsPageIcon.png" alt="Notices">
+                        <img onMouseOver="swapDark('2')" onMouseOut="swapLight('2')" class="botImg" src="<?php echo base_url(); ?>img/WA_events.png" alt="Events">
                         <p class="dashboardText"> Events </p>    
                     </a>
                 </div>
