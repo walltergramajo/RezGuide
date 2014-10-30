9fee904a8ea99c40917cbdf52bb1a4e596c2c88d
walltergramajo <waltergramajo@yahoo.ca>
2014-10-30 16:41:44 -0400

    Main Menu and Footer Icon Change
    Changed specified icons to the dark color icons with the red hover.

--- RezGuide/application/views/templates/footer.php
+++ RezGuide/application/views/templates/footer.php
@@ -4,25 +4,25 @@
           	<nav class="row">
           		<div class="small-3 columns">
                 	<a class="botAnchor" href="<?php echo base_url(); ?>index.php/main">
-                	<img  onMouseOver="swapDark1('0')" onMouseOut="swapLight1('0')" class="botImg1" src="<?php echo base_url(); ?>img/homeIcon.png" alt="Home">
+                	<img  onMouseOver="swapDark1('0')" onMouseOut="swapLight1('0')" class="botImg1" src="<?php echo base_url(); ?>img/WA_homeBut.png" alt="Home">
                     <p class="menuText header">Home</p>
                     </a>
                 </div>
                 <div class="small-3 columns">
                 	<a class="botAnchor" href="<?php echo base_url(); ?>index.php/chat">
-                	<img onMouseOver="swapDark1('1')" onMouseOut="swapLight1('1')" class="botImg1" src="<?php echo base_url(); ?>img/chatIcon.png" alt="Chat">
+                	<img onMouseOver="swapDark1('1')" onMouseOut="swapLight1('1')" class="botImg1" src="<?php echo base_url(); ?>img/WA_chatBut.png" alt="Chat">
                     <p class="menuText header">Chat</p>
                     </a>
                 </div>
                 <div class="small-3 columns">
                 	<a class="botAnchor" href="<?php echo base_url(); ?>index.php/dashboard/account">
-                	<img onMouseOver="swapDark1('2')" onMouseOut="swapLight1('2')" class="botImg1" src="<?php echo base_url(); ?>img/settingIcon.png" alt="Settings">
+                	<img onMouseOver="swapDark1('2')" onMouseOut="swapLight1('2')" class="botImg1" src="<?php echo base_url(); ?>img/WA_settingsBut.png" alt="Settings">
                     <p class="menuText header">Settings</p>
                     </a>
                 </div>
                 <div class="small-3 columns">
                 	<a class="botAnchor" href="<?php echo base_url(); ?>index.php/login/logout">
-                	<img onMouseOver="swapDark1('3')" onMouseOut="swapLight1('3')" class="botImg1" src="<?php echo base_url(); ?>img/logoutIcon.png" alt="Logout">
+                	<img onMouseOver="swapDark1('3')" onMouseOut="swapLight1('3')" class="botImg1" src="<?php echo base_url(); ?>img/WA_logoutBut.png" alt="Logout">
                     <p class="menuText header">Logout</p>
                     </a>
                 </div>
