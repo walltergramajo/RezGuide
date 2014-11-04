fad53f67e300e7ce73198e37cd6065f7a46f4dfd
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-11-04 18:27:14 -0500

    Added event data to calendar
    Added data echo for calendar view and changed result type in calendar
    model

--- RezGuide/application/models/calendar_model.php
+++ RezGuide/application/models/calendar_model.php
@@ -4,7 +4,7 @@ class Calendar_model extends CI_Model {
 
 	public function getEvents(){
 		$student = $this->db->get_where('tbl_studentsevents', array('students_id' => $this->session->userdata('sId')));
-		$studentEvents = $student->result_array();
+		$studentEvents = $student->row_array();
 		$calEvents = $this->db->get_where('tbl_events', array('events_id' => $studentEvents['events_id']));
 		return $calEvents->result_array();
 	}
--- RezGuide/application/views/calendar/calendar.php
+++ RezGuide/application/views/calendar/calendar.php
@@ -4,27 +4,29 @@
             <div class="small-4 columns text-center">
                 <div id="calendarAccordion">
                     <dl class="accordion" data-accordion>
-                        <dd>
-                            <a href="#panel1" class="accordionLink" onClick="highlightLink(this);">Content 1 <span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
-                            <div id="panel1" class="content">
-                                <div class="dividerAccordionTop"></div>
-                                <div class="row eventInfo">
-                                    <div class=" small-7 columns text-center" >
-                                        <img src="<?php echo base_url(); ?>img/eventNumber.png">
-                                    </div>
-                                    <div class=" small-5 columns" >
-                                        <p class="eventStart"> 5:00 pm</p>
-                                        <p class="eventEnd"> 5:30 pm </p>
+                        <?php foreach ($events as $row): ?>
+                            <dd>
+                                <a href="#panel1" class="accordionLink" onClick="highlightLink(this);"><?php echo $row['events_title'] ?><span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
+                                <div id="panel1" class="content">
+                                    <div class="dividerAccordionTop"></div>
+                                    <div class="row eventInfo">
+                                        <div class=" small-7 columns text-center" >
+                                            <img src="<?php echo base_url(); ?>img/eventNumber.png">
+                                        </div>
+                                        <div class=" small-5 columns" >
+                                            <p class="eventStart"> 5:00 pm</p>
+                                            <p class="eventEnd"> 5:30 pm </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="small-12 columns">
-                                    <div class="dividerAccordion"></div>
+                                <div class="row">
+                                    <div class="small-12 columns">
+                                        <div class="dividerAccordion"></div>
+                                    </div>
                                 </div>
-                            </div>
-                        </dd>
-                        <dd>
+                            </dd>
+                        <?php endforeach; ?>
+                        <!-- <dd>
                             <a href="#panel2" class="accordionLink" onClick="highlightLink(this);">Content 2 <span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
                             <div id="panel2" class="content">
                                 <div class="dividerAccordionTop"></div>
@@ -63,7 +65,7 @@
                                     <div class="dividerAccordion"></div>
                                 </div>
                             </div>
-                        </dd>
+                        </dd> -->
                     </dl>
                 </div>
             </div>
