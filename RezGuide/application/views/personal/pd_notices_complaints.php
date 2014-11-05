d4f8b778c0482f1a3417e6ac1b34f45c238b8c17
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-11-05 10:27:26 -0500

    Added missing 'echo's
    Found missing echo declarations aded them in to fix the page

--- RezGuide/application/views/personal/pd_notices_complaints.php
+++ RezGuide/application/views/personal/pd_notices_complaints.php
@@ -11,34 +11,13 @@
                 	<dl class="accordion" data-accordion>
                         <?php foreach ($results as $row): ?>
                         	<dd class="accordionBottom">
-                                <a href="#panel<?php $row->notices_id ?>" class="accordionLink accordionTitle" onClick="highlightLink2(this);"><?php $row->notices_title ?><span class="right accordionDropDownIcon"><img class="imgswap" src="<?php echo base_url(); ?>img/accordionArrowDown.png" alt="Click For more Information."></span></a>
-                                <div id="panel<?php $row->notices_id ?>" class="content">
-                                    <p class="complaintDate"><?php $row->notices_date ?></p>
-                                    <p><?php $row->notices_description ?></p>
+                                <a href="#panel<?php echo $row->notices_id ?>" class="accordionLink accordionTitle" onClick="highlightLink2(this);"><?php echo $row->notices_title ?><span class="right accordionDropDownIcon"><img class="imgswap" src="<?php echo base_url() ?>img/accordionArrowDown.png" alt="Click For more Information."></span></a>
+                                <div id="panel<?php echo $row->notices_id ?>" class="content">
+                                    <p class="complaintDate"><?php  echo $row->notices_date ?></p>
+                                    <p><?php echo $row->notices_description ?></p>
                                 </div>
                             </dd>
                         <?php endforeach; ?>
-                        <!-- <dd class="accordionBottom">
-                            <a href="#panel2" class="accordionLink accordionTitle" onClick="highlightLink2(this);">Exceeded Alcohol Limit <span class="right accordionDropDownIcon"><img class="imgswap" src="img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
-                            <div id="panel2" class="content"> 
-                                <p class="complaintDate">September 16, 2014</p>
-                                <p>Residence limit of alcoho allowed is 1L per student and this has been violated by possessing 3L of alcohol within the Residence.</p>
-                            </div>
-                        </dd>
-                        <dd class="accordionBottom">
-                            <a href="#panel3" class="accordionLink accordionTitle" onClick="highlightLink2(this);">Guests Violation <span class="right accordionDropDownIcon"><img class="imgswap" src="img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
-                            <div id="panel3" class="content"> 
-                                <p class="complaintDate">September 12, 2014</p>
-                                <p>Student is only allowed to sign in 1 guest. This has been violated by sneaking in 2 other guests for the night.</p>
-                            </div>
-                        </dd>
-                        <dd class="accordionBottom">
-                            <a href="#panel4" class="accordionLink accordionTitle" onClick="highlightLink2(this);">Residence Rules Violation <span class="right accordionDropDownIcon"><img class="imgswap" src="img/accordionArrowDown.png" alt="Click For more Information."></span> </a>
-                            <div id="panel4" class="content">
-                                <p class="complaintDate">September 12, 2014</p>
-                                <p>Student has failed to follow several rules put in place by Fanshawe Residence. The next violation might result in being removed from Residence all-together.</p>
-                            </div>
-                        </dd> -->
                     </dl>
                 </div>
             </div>
