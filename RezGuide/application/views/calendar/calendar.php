58bfcd942e24724de3430e81e64e05fc1c38e512
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-11-05 12:52:17 -0500

    Changes to Calendar for event data
    Added new view and changed model and controller

--- RezGuide/application/views/calendar/calendar.php
+++ RezGuide/application/views/calendar/calendar.php
@@ -6,16 +6,16 @@
                     <dl class="accordion" data-accordion>
                         <?php foreach ($events as $row): ?>
                             <dd>
-                                <a href="#panel1" class="accordionLink" onClick="highlightLink(this);"><?php echo $row['events_title'] ?><span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
-                                <div id="panel1" class="content">
+                                <a href="#panel<?php echo $row->events_id ?>" class="accordionLink" onClick="highlightLink(this);"><?php echo $row->events_title ?><span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
+                                <div id="panel<?php echo $row->events_id ?>" class="content">
                                     <div class="dividerAccordionTop"></div>
                                     <div class="row eventInfo">
                                         <div class=" small-7 columns text-center" >
                                             <img src="<?php echo base_url(); ?>img/eventNumber.png">
                                         </div>
                                         <div class=" small-5 columns" >
-                                            <p class="eventStart"> 5:00 pm</p>
-                                            <p class="eventEnd"> 5:30 pm </p>
+                                            <p class="eventStart"><?php echo $row->events_startdate ?></p>
+                                            <p class="eventEnd"><?php echo $row->events_enddate ?></p>
                                         </div>
                                     </div>
                                 </div>
@@ -26,46 +26,6 @@
                                 </div>
                             </dd>
                         <?php endforeach; ?>
-                        <!-- <dd>
-                            <a href="#panel2" class="accordionLink" onClick="highlightLink(this);">Content 2 <span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
-                            <div id="panel2" class="content">
-                                <div class="dividerAccordionTop"></div>
-                                <div class="row eventInfo">
-                                    <div class=" small-7 columns text-center" >
-                                        <img src="<?php echo base_url(); ?>img/eventNumber.png">
-                                    </div>
-                                     <div class=" small-5 columns" >
-                                        <p class="eventStart"> 5:00 pm</p>
-                                        <p class="eventEnd"> 5:30 pm </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="small-12 columns">
-                                    <div class="dividerAccordion"></div>
-                                </div>
-                            </div>
-                        </dd>
-                        <dd>
-                            <a href="#panel3" class="accordionLink" onClick="highlightLink(this);">Content 3 <span class="right"><img src="<?php echo base_url(); ?>img/accordionArrowDownHover.png" alt="Click For more Information."></span></a>
-                            <div id="panel3" class="content">
-                                <div class="dividerAccordionTop"></div>
-                                <div class="row eventInfo">
-                                    <div class=" small-7 columns text-center" >
-                                        <img src="<?php echo base_url(); ?>img/eventNumber.png">
-                                    </div>
-                                    <div class=" small-5 columns" >
-                                        <p class="eventStart"> 5:00 pm</p>
-                                        <p class="eventEnd"> 5:30 pm </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="small-12 columns">
-                                    <div class="dividerAccordion"></div>
-                                </div>
-                            </div>
-                        </dd> -->
                     </dl>
                 </div>
             </div>
