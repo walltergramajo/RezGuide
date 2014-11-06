83e94089e15ec412e111b7d94046f5e84a492f5f
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-11-06 17:27:50 -0500

    Added calls to get student data for edit page
    Added function and call to account model to fetch student data based on
    session id to fill in defaults for account edit views.

--- RezGuide/application/models/account_model.php
+++ RezGuide/application/models/account_model.php
@@ -10,14 +10,14 @@ class Account_model extends CI_Model {
 		$query = "SELECT * FROM tbl_pointsstudent, tbl_points WHERE tbl_pointsstudent.points_id = tbl_points.points_id AND tbl_pointsstudent.students_id = ".$sId;
 		$points = $this->db->query($query);
 		return $points->result();
-
-		// $student = $this->db->get_where('tbl_pointsstudent', array('students_id' => $sId));
-		// $studentPoints = $student->result();
-		// $points = $this->db->get_where('tbl_points', array('points_id' => $studentPoints->points_id));
-		// return $points->result();
 	}
 
 	public function totalPoints($sId){
 		
+	}
+
+	public function studentData($sId){
+		$query = $this->db->get_where('tbl_students', array('students_id' => $sId));
+		return $query->result_array();
 	}
 }
