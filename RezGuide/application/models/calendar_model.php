2341699eb64cf43b8f603c35b25e34da13f45967
G3X787 <d_krent@TSS-HR312-DM31.local>
2014-11-12 11:02:50 -0500

    Changed query string to fix always returning null
    Changed query string to include ‘’ around $date, now correctly queries
    database.

--- RezGuide/application/models/calendar_model.php
+++ RezGuide/application/models/calendar_model.php
@@ -3,8 +3,8 @@
 class Calendar_model extends CI_Model {
 
 	public function getEvents($date){
-		$query = "SELECT * FROM tbl_studentsevents, tbl_events WHERE tbl_studentsevents.events_id = tbl_events.events_id AND tbl_studentsevents.students_id = ".$this->session->userdata('sId')." AND tbl_events.events_startdate = ".$date;
+		$query = "SELECT * FROM tbl_studentsevents, tbl_events WHERE tbl_studentsevents.events_id = tbl_events.events_id AND tbl_studentsevents.students_id = ".$this->session->userdata('sId')." AND tbl_events.events_caldate = '".$date."'";
 		$calEvents = $this->db->query($query);
-		return $calEvents->result_array();
+		return $calEvents->result();
 	}
 }
