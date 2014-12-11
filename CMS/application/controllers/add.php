4a882f09619e6f7157dc03ab40505e4a49a402d5
walltergramajo <waltergramajo@yahoo.ca>
2014-12-11 16:32:44 -0500

    Changed General Info Structure
    Changed the General Info section to the new structure, still need to add
    the Edit and Add options for each.

--- CMS/application/controllers/add.php
+++ CMS/application/controllers/add.php
@@ -1039,27 +1039,36 @@ class Add extends CI_Controller {
 		if($page == "city"){
 			$data['pgTitle'] = "RezGuide Add A City Location";
 			$data['section'] = "General";
+			$data['ZoneTitle'] = "Add";
+			$data['sectTitle'] = "City Info";
 			$data['subSection'] = "City Locations";
 			$this->load->view('templates/head',$data);
 			$this->load->view('add/add_header');
+			$this->load->view('general/general_options_menu');
 			$this->load->view('add/general/city');
 			$this->load->view('templates/footer');
 			$this->load->view('templates/close');
 		}elseif($page == "directory"){
 			$data['pgTitle'] = "RezGuide Add A User to Directory";
 			$data['section'] = "General";
+			$data['ZoneTitle'] = "Add";
+			$data['sectTitle'] = "Directory Info";
 			$data['subSection'] = "Directory";
 			$this->load->view('templates/head',$data);
 			$this->load->view('add/add_header');
+			$this->load->view('general/general_options_menu');
 			$this->load->view('add/general/directory');
 			$this->load->view('templates/footer');
 			$this->load->view('templates/close');
 		}elseif($page == "information"){
 			$data['pgTitle'] = "RezGuide Add LTC Information";
 			$data['section'] = "General";
+			$data['ZoneTitle'] = "Add";
+			$data['sectTitle'] = "General Info";
 			$data['subSection'] = "LTC Information";
 			$this->load->view('templates/head',$data);
 			$this->load->view('add/add_header');
+			$this->load->view('general/general_options_menu');
 			$this->load->view('add/general/information');
 			$this->load->view('templates/footer');
 			$this->load->view('templates/close');
