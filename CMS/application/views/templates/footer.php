01c561dc896b2b6ff8f13d656612cd96be8f0869
walltergramajo <waltergramajo@yahoo.ca>
2014-12-11 16:36:46 -0500

    Footer Change
    Removed the Add and Edit Options from the footer as they are now
    accessible from the main menu for each option.

--- CMS/application/views/templates/footer.php
+++ CMS/application/views/templates/footer.php
@@ -6,11 +6,11 @@
                     <ul id="footerNav">
                         <li>
                             <a class="botAnchor" href="<?php echo base_url(); ?>index.php/main">
-                            <img onMouseOver="swapDark('3')" onMouseOut="swapLight('3')" class="botImg" src="<?php echo base_url(); ?>img/CMS_home.png" alt="Home">
+                            <img onMouseOver="swapDark('5')" onMouseOut="swapLight('5')" class="botImg" src="<?php echo base_url(); ?>img/CMS_home.png" alt="Home">
                             <p class="menuText header">Home</p>
                             </a>
                         </li>
-                        <li>
+                        <!-- <li>
                             <a class="botAnchor" href="<?php echo base_url(); ?>index.php/add">
                             <img onMouseOver="swapDark('4')" onMouseOut="swapLight('4')" class="botImg" src="<?php echo base_url(); ?>img/CMS_add.png" alt="Add">
                             <p class="menuText header">Add</p>
@@ -21,7 +21,7 @@
                             <img onMouseOver="swapDark('5')" onMouseOut="swapLight('5')" class="botImg" src="<?php echo base_url(); ?>img/CMS_edit.png" alt="Edit">
                             <p class="menuText header">Edit</p>
                             </a>
-                        </li>
+                        </li> -->
                         <li>
                             <a class="botAnchor" href="<?php echo base_url(); ?>index.php/users">
                             <img onMouseOver="swapDark('6')" onMouseOut="swapLight('6')" class="botImg" src="<?php echo base_url(); ?>img/CMS_users.png" alt="Users">
@@ -29,8 +29,14 @@
                             </a>
                         </li>
                         <li>
+                            <a class="botAnchor" href="<?php echo base_url(); ?>index.php/">
+                            <img onMouseOver="swapDark('7')" onMouseOut="swapLight('7')" class="botImg" src="<?php echo base_url(); ?>img/WA_requests.png" alt="Edit">
+                            <p class="menuText header">Requests</p>
+                            </a>
+                        </li> 
+                        <li>
                             <a class="botAnchor" href="<?php echo base_url(); ?>index.php/login/logout">
-                            <img onMouseOver="swapDark('7')" onMouseOut="swapLight('7')" class="botImg" src="<?php echo base_url(); ?>img/CMS_logout.png" alt="Logout">
+                            <img onMouseOver="swapDark('8')" onMouseOut="swapLight('8')" class="botImg" src="<?php echo base_url(); ?>img/CMS_logout.png" alt="Logout">
                             <p class="menuText header">Logout</p>
                             </a>
                         </li>
